e779308cb7697d93cded81feba12fb66cd9a1749
Subodh <sohonisubodh@gmail.com>
2023-12-16 00:12:12 +0530

    added create user functionality

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const port=process.env.PORT || 5000;
@@ -14,8 +15,6 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+app.use("/api/users",userRoutes);
 
-app.get("/",(req,res)=>{
-    res.send("HELLO");
-})
 app.listen(port,()=>console.log(`Server running on port : ${port}`));
